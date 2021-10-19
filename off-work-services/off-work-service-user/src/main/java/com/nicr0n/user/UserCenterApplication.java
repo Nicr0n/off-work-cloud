c3f099f86956697e947bc04c063d8a7a86f5c8a2
Nicron <845182672@qq.com>
2021-10-19 17:46:17 +0800

    修改配置文件

---
+++ off-work-services/off-work-service-user/src/main/java/com/nicr0n/user/UserCenterApplication.java
@@ -0,0 +1,12 @@
+package com.nicr0n.user;
+
+/**
+ * @author: Nicr0n
+ * @date: 2021/10/19 17:35
+ * @email: Nicr0nFF@gmail.com
+ */
+public class UserCenterApplication {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
