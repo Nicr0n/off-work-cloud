58918056e0e15bb9c9370568d736135dcfb45548
Nicron <845182672@qq.com>
2021-10-19 17:56:59 +0800

    修改配置文件

--- off-work-services/off-work-service-user/src/main/java/com/nicr0n/user/UserCenterApplication.java
+++ off-work-services/off-work-service-user/src/main/java/com/nicr0n/user/UserCenterApplication.java
@@ -1,12 +1,16 @@
 package com.nicr0n.user;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author: Nicr0n
  * @date: 2021/10/19 17:35
  * @email: Nicr0nFF@gmail.com
  */
+@SpringBootApplication
 public class UserCenterApplication {
     public static void main(String[] args) {
-        System.out.println(1);
+        SpringApplication.run(UserCenterApplication.class);
     }
 }
