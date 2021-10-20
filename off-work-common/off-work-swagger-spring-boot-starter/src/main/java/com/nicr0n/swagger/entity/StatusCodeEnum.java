c277e11ef7812759bf57c1dc838aa672328781c3
Nicron <845182672@qq.com>
2021-10-21 00:42:11 +0800

    创建RESTful风格通用返回对象

---
+++ off-work-common/off-work-swagger-spring-boot-starter/src/main/java/com/nicr0n/swagger/entity/StatusCodeEnum.java
@@ -0,0 +1,32 @@
+package com.nicr0n.swagger.entity;
+
+import lombok.Getter;
+
+/**
+ * @author: Nicr0n
+ * @date: 2021/10/21  0:06
+ * @email: Nicr0nFF@gmail.com
+ */
+@Getter
+/**
+ * 返回码枚举类
+ */
+public enum StatusCodeEnum{
+	REQUIRE_SUCCESS("200","请求成功"),
+	REQUIRE_FAIL_INTERNAL("500","请求失败，服务器内部错误！请检查控制台日志。");
+
+	/**
+	 * http状态码
+	 */
+	private String code;
+
+	/**
+	 * 消息
+	 */
+	private String message;
+
+	StatusCodeEnum(String code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+}
